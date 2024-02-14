fd347cc83c3c3731e224b60ffbdea8a69eeccd01
Jack Shieh <71810019+sme322-ui@users.noreply.github.com>
2024-02-14 15:54:34 +0800

    Create RecipesTableViewController.swift
    Signed-off-by: Jack Shieh <71810019+sme322-ui@users.noreply.github.com>

---
+++ MyMap3/Views/Model/RecipesTableViewController.swift
@@ -0,0 +1,66 @@
+//  Copyright © 2016 Bilal ARSLAN. All rights reserved.
+//
+
+import UIKit
+
+class RecipesTableViewController: UITableViewController {
+
+    private var recipes = Recipe.createRecipes()
+    private let cellIdentifier: String = "tableCell"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    // MARK: Segue Method
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "recipeDetail",
+            let indexPath = tableView?.indexPathForSelectedRow,
+            let destinationViewController: DetailViewController = segue.destination as? DetailViewController {
+            destinationViewController.recipe = recipes[indexPath.row]
+        }
+    }
+
+}
+
+extension RecipesTableViewController {
+
+    private func setupUI() {
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .done, target: self, action: nil)
+        navigationItem.title = "Recipes"
+        tableView.reloadData()
+    }
+
+}
+
+// MARK: - UITableView DataSource
+
+extension RecipesTableViewController {
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipes.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as? TableCell {
+            cell.configurateTheCell(recipes[indexPath.row])
+            return cell
+        }
+        return UITableViewCell()
+    }
+
+}
+
+// MARK: - UITableView Delegate
+
+extension RecipesTableViewController {
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            recipes.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .bottom)
+        }
+    }
+
+}
