d88f9a45a7aefb1371d7ee220d519b3401c28dbf
Jack Shieh <71810019+sme322-ui@users.noreply.github.com>
2024-02-14 15:39:59 +0800

    Create WebViewController.swift
    Signed-off-by: Jack Shieh <71810019+sme322-ui@users.noreply.github.com>

---
+++ MyMap3/WebViewController.swift
@@ -0,0 +1,43 @@
+//
+//  WebViewController.swift
+//  MyMap4
+//
+//  Created by Mongyan on 2023/5/1.
+//
+
+import Foundation
+import WebKit
+import OpenAI
+import OpenAISwift
+import PythonKit
+import SwiftUI
+class WebViewController :UIViewController,WKNavigationDelegate{
+    var webView: WKWebView!
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let ipAddress = "192.168.1.104"
+               
+               // 拼接完整的URL
+               if let url = URL(string: "http://\(ipAddress)") {
+                   
+                   // 使用UIApplication打開URL
+                   if UIApplication.shared.canOpenURL(url) {
+                       UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                       print("打開URL")
+                   } else {
+                       print("無法打開URL")
+                   }
+               }
+        
+    }
+    
+    
+    
+}
