fd43dae535daceb7217230a96194d94d09005ae2
Jack Shieh <71810019+sme322-ui@users.noreply.github.com>
2024-03-23 20:55:37 +0800

    Update BLEManager.swift
    Signed-off-by: Jack Shieh <71810019+sme322-ui@users.noreply.github.com>

--- MyMap3/BLEManager.swift
+++ MyMap3/BLEManager.swift
@@ -10,8 +10,8 @@ import Foundation
 import CoreBluetooth
 
 private struct BLEConstants {
-    static let TemperatureService = CBUUID(string: "6e400001-b5a3-f393-e0a9-e50e24dcca9e")
-    static let RXCharacteristic = CBUUID(string: "6e400003-b5a3-f393-e0a9-e50e24dcca9e")
+    static let TemperatureService = CBUUID(string: "")
+    static let RXCharacteristic = CBUUID(string: "")
 }
 
 private struct Weak<T: AnyObject> {
